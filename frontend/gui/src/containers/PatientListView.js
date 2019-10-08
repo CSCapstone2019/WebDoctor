23f081afb74bffaa4dbf819531113c8a98024beb
Josh Arrants <duhhobo@gmail.com>
2019-10-08 15:05:22 -0400

    changes to PatientListView

--- frontend/gui/src/containers/PatientListView.js
+++ frontend/gui/src/containers/PatientListView.js
@@ -1,24 +1,64 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-// import Patients from '../components/Patients';
-import PatientLists from '../components/PatientList';
+import AppNavbar from '../components/AppNavbar';
+import {
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  CardDeck,
+  CardSubtitle,
+  CardBody,
+  CardImg,
+  Row,
+  Col,
+  Container
+} from 'reactstrap';
 
 class PatientList extends Component {
   state = {
     patients: []
   };
 
-  componentDidMount() {
-    axios.get('http://127.0.0.1:8000/api/patient/').then(res => {
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/patient/');
+      const patients = await res.json();
       this.setState({
-        patients: res.data
+        patients
       });
-      // console.log(res.data);
-    });
+      // console.log(patients);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {
-    return <PatientLists data={this.state.patients} />;
+    return (
+      <>
+        <AppNavbar />
+        {this.state.patients.map(item => (
+          <Container key={item.patient_id}>
+            <CardDeck>
+              <Card>
+                {/* <CardImg top width="100%" src="/assets/256x186.svg" alt="Card image cap" /> */}
+                <CardBody>
+                  <CardTitle>{`${item.first_name} ${item.last_name}`}</CardTitle>
+                  <CardSubtitle>
+                    <strong>Email:</strong> {item.email}
+                  </CardSubtitle>
+                  <br />
+                  <CardSubtitle>
+                    <strong>Email:</strong> {item.email}
+                  </CardSubtitle>
+                  <CardText>text</CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+            </CardDeck>
+          </Container>
+        ))}
+      </>
+    );
   }
 }
 
