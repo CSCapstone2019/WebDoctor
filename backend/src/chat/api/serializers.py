04cf0ea6edc34c1225ddfbca201abbbc7887e2c1
augichii <augichii@gmail.com>
2019-11-20 18:46:48 -0500

    fixing backend

--- backend/src/chat/api/serializers.py
+++ backend/src/chat/api/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
 
 from patients.models import Chat
+from chat.views import get_user_contact
+
+
+class ContactSerializer(serializers.StringRelatedField):
+    def to_internal_value(self, value):
+        return value
 
 
 class ChatSerializer(serializers.ModelSerializer):
+    participants = ContactSerializer(many=True)
+
     class Meta:
         model = Chat
-        fields = ('__all__') 
+        fields = ('chat_id', 'messages', 'participants')
+        read_only = ('chat_id')
+
+    def create(self, validated_data):
+        print(validated_data)
+        participants = validated_data.pop('participants')
+        chat = Chat()
+        chat.save()
+        for username in participants:
+            contact = get_user_contact(username)
+            chat.participants.add(contact)
+        chat.save()
+        return chat
