c8f6d16b03b8a1c5b785693a72ba55064122a2ff
augichii <augichii@gmail.com>
2019-12-01 11:12:04 -0500

    added and configured the calendar frontend component. just need to pass in the right data into it

--- frontend/gui/src/components/appointment/Schedule.js
+++ frontend/gui/src/components/appointment/Schedule.js
@@ -4,27 +4,30 @@ import PropTypes from 'prop-types';
 import * as scheduleActions from '../../store/actions/schedule';
 import { Button, Container, Table } from 'reactstrap';
 import Contact from '../chat/ChatContact';
+import { Calendar, Badge } from "antd";
+import moment from "moment";
+
 
 class Schedule extends React.Component {
-
-  // state = {
-  //   all_schedules: [],
-  //   activeSchedule: null,
-  // }
+  state = {
+    listDate: []
+  };
   static propTypes = {
     auth: PropTypes.object.isRequired,
     isLoading: PropTypes.bool.isRequired,
     loading: PropTypes.bool.isRequired,
-    getUserSchedule: PropTypes.func.isRequired,
+    getUserSchedule: PropTypes.func.isRequired
   };
 
-  
   waitForDetail() {
     const component = this;
-    console.log("-------------IS LOADING SHCEUDLE: ", component.props.isLoading)
-    setTimeout(function () {
-      if (!(component.props.isLoading)) {
-        console.log("-------------USER: ", component.props.auth.user.username)
+    console.log(
+      "-------------IS LOADING SHCEUDLE: ",
+      component.props.isLoading
+    );
+    setTimeout(function() {
+      if (!component.props.isLoading) {
+        console.log("-------------USER: ", component.props.auth.user.username);
         component.props.getUserSchedule(component.props.auth.user.username); //component.props.auth.user.username
         return;
       } else {
@@ -34,31 +37,98 @@ class Schedule extends React.Component {
     }, 100);
   }
 
-
   componentDidMount() {
     this.waitForDetail();
   }
 
+  // CALENDAR
+  getListData = value => {
+    console.log("GET LIST DATA VALUE: ", value);
+    let listData;
+    switch (value.date()) {
+      case 8:
+        listData = [
+          { type: "warning", content: "This is warning event." },
+          { type: "success", content: "This is usual event." }
+        ];
+        break;
+      case 10:
+        listData = [
+          { type: "warning", content: "This is warning event." },
+          { type: "success", content: "This is usual event." },
+          { type: "error", content: "This is error event." }
+        ];
+        break;
+      case 15:
+        listData = [
+          { type: "warning", content: "This is warning event" },
+          { type: "success", content: "This is very long usual event。。...." },
+          { type: "error", content: "This is error event 1." },
+          { type: "error", content: "This is error event 2." },
+          { type: "error", content: "This is error event 3." },
+          { type: "error", content: "This is error event 4." }
+        ];
+        break;
+      default:
+    }
+    return listData || [];
+  };
+
+  dateCellRender = value => {
+    console.log("DATE CELL RENDER VALUE:", value);
+    const listData = this.getListData(value);
+    return (
+      <ul className="events">
+        {listData.map(item => (
+          <li key={item.content}>
+            <Badge status={item.type} text={item.content} />
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  getMonthData = value => {
+    if (value.month() === 8) {
+      return 1394;
+    }
+  };
+
+  monthCellRender = value => {
+    const num = this.getMonthData(value);
+    return num ? (
+      <div className="notes-month">
+        <section>{num}</section>
+        <span>Backlog number</span>
+      </div>
+    ) : null;
+  };
+
+  onPanelChange = (value, mode) => {
+    console.log(value, mode);
+  };
+  //CALENDAR END
+
+
   render() {
+    // const { value, selectedValue } = this.state;
     let activeSchedule = this.props.schedules.map(c => {
       return (
-              <tr key={c.schedule_id}>
-                <td>{c.schedule_id}</td>
-                <td>{c.participants.toString()}</td>
-                <td>{c.appointment_date}</td>
-                <td>{c.appointment_time}</td>
-                <td>{c.message}</td>
-              </tr>
-          
+        <tr key={c.schedule_id}>
+          <td>{c.schedule_id}</td>
+          <td>{c.participants.toString()}</td>
+          <td>{c.appointment_date}</td>
+          <td>{c.appointment_time}</td>
+          <td>{c.message}</td>
+        </tr>
       );
     });
 
     console.log("SCHEDULE ", this.props.schedules);
 
     return (
-      
       <>
-      <Container>
+        <Container>
           <Table dark hover responsive>
             <thead>
               <tr>
@@ -70,55 +140,25 @@ class Schedule extends React.Component {
                 <th />
               </tr>
             </thead>
-            <tbody>
-              {activeSchedule}
-            </tbody>
+            <tbody>{activeSchedule}</tbody>
           </Table>
-      </Container>
+        </Container>
+        <Container>
+          <div
+            style={{
+              width: "100%",
+              border: "1px solid #d9d9d9",
+              borderRadius: 4
+            }}
+          >
+            <Calendar
+              dateCellRender={this.dateCellRender}
+              monthCellRender={this.monthCellRender}
+              onPanelChange={this.onPanelChange}
+            />
+          </div>
+        </Container>
       </>
-      // <>
-      //   <Container>
-      //     <h2 className="text-center">
-      //       <strong>Appointments</strong>
-      //     </h2>
-      //     <br />
-      //     <Table dark hover responsive>
-      //       <thead>
-      //         <tr>
-      //           <th>#</th>
-      //           <th>Patient</th>
-      //           <th>Appointment Date</th>
-      //           <th>Appointment Time</th>
-      //           <th>Message</th>
-      //           <th />
-      //         </tr>
-      //       </thead>
-      //       <tbody>
-      //         {this.props.appointments.map(a => (
-      //           <tr key={a.appointment_id}>
-      //             <td>{a.appointment_id}</td>
-      //             <td>{a.patient}</td>
-      //             <td>{a.appointment_date}</td>
-      //             <td>{a.appointment_time}</td>
-      //             <td>{a.message}</td>
-      //             <td>
-      //               <Button
-      //                 onClick={this.props.deleteAppointment.bind(
-      //                   this,
-      //                   a.appointment_id
-      //                 )}
-      //                 color="danger"
-      //                 size="sm"
-      //               >
-      //                 Delete
-      //               </Button>
-      //             </td>
-      //           </tr>
-      //         ))}
-      //       </tbody>
-      //     </Table>
-      //   </Container>
-      // </>
     );
   }
 }
